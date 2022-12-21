e8173eb0e3ebb20be11f0d1a87b504541dd07d64
dridiyassin <dridiyassin@gmail.com>
2022-12-21 17:23:00 +0100

    Simple Player Movements + Animation

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float walkSpeed;
+
+    Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    Vector2 movement;
+
+    float currentDirection;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.MovePosition(rb.position + movement * walkSpeed * Time.fixedDeltaTime);
+        float WalkDir = GetDirection();
+        if (movement != Vector2.zero)
+        {
+            animator.SetBool("Walking", true);
+            animator.SetFloat("Walking_Dir", WalkDir);
+        } else
+        {
+            animator.SetBool("Walking", false);
+        }
+
+        //Player Flips when walk left
+        if (WalkDir == 3)
+            spriteRenderer.flipX = true;
+        else
+            spriteRenderer.flipX = false;
+    }
+
+    public float GetDirection()
+    {
+
+        if(movement.x >= 1)
+            currentDirection = 2;
+        else if(movement.x < 0)
+            currentDirection = 3;
+
+        if (movement.y >= 1)
+            currentDirection = 1;
+        else if(movement.y < 0)
+            currentDirection = 0;
+        
+        return currentDirection;
+    }
+}
