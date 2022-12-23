6a02768f68f451a3acb1f945631125e8b40d6631
dridiyassin <dridiyassin@gmail.com>
2022-12-23 01:09:58 +0100

    Update Inventory.cs

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public List<Clothing> clothingInventory = new List<Clothing>();
+    public List<Item> itemsInventory = new List<Item>();
+
+
+    public static Inventory Instance;
+    void Awake()
     {
-        
+        if (Instance == null) Instance = this;
     }
+
+
+    
+
 
     // Update is called once per frame
     void Update()
