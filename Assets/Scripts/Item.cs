b20dca2fb4b3fe7e56a202eef68ebd5cb3868c7f
dridiyassin <dridiyassin@gmail.com>
2022-12-23 01:05:24 +0100

    UI prototype

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item", order = 1)]
+public class Item : ScriptableObject
+{
+    public float sellPrice;
+    public float buyPrice;
+}
